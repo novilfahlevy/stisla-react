db88ce96389d959018febf975faaaf7ba310e1d1
novilfahlevy <novilfreon@gmail.com>
2020-08-21 17:42:47 +0800

    fix routes in auth

--- src/layouts/Auth.jsx
+++ src/layouts/Auth.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import {
   Container
@@ -26,7 +26,7 @@ class Auth extends React.Component {
   render() {
     return (
       <Container className="mt-5">
-        {this.getRoutes(routes)}
+        <Switch>{this.getRoutes(routes)}</Switch>
       </Container>
     );
   }
